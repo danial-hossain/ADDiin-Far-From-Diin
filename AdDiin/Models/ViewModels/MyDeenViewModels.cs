96b0f9c24c936d5e9ab8cc126d61650953f9d51c
Yamesiuu70 <toufikul.alam30@gmail.com>
2026-09-19 01:58:03 +0600

    feat: wire daily Hadith into My Deen

--- AdDiin/Models/ViewModels/MyDeenViewModels.cs
+++ AdDiin/Models/ViewModels/MyDeenViewModels.cs
@@ -1,4 +1,5 @@
 ﻿using AdDiin.Models.Entities;
+using AdDiin.Models;
 
 namespace AdDiin.Models.ViewModels
 {
@@ -13,6 +14,7 @@ namespace AdDiin.Models.ViewModels
         public List<AdhkarItemViewModel> EveningAdhkar { get; set; } = new();
         public List<RuqyahItemViewModel> RuqyahRoutines { get; set; } = new();
         public List<DailyProgressSummary> WeeklyProgress { get; set; } = new();
+        public IReadOnlyList<Hadith> DailyHadiths { get; set; } = Array.Empty<Hadith>();
         
         public int TotalDhikrCountToday { get; set; }
         public int DailyProgressPercent { get; set; }
